3731a46537f9e2f61915769b734ced6bdc1d57be
AlexTumbaco <tumbaco.posligua.alex@gmail.com>
2025-03-11 21:00:45 -0500

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la 
+// lógica para resolver el problema.
+
+
+let listaAmigos = [];
+
+function agregarAmigo () {
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if ( nombreAmigo != '' ) {
+        listaAmigos.push(nombreAmigo);
+        console.log (listaAmigos);
+        nombreAmigo = document.getElementById("amigo").value= '';
+    } else {
+        alert ("Por favor, inserte un nombre");
+    }
+}
